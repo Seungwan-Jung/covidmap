5b016c75cfd135b30e6e76ab884ec73b38c18242
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2021-06-13 11:47:30 +0900

    2.0 introduction of dash

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+
+app = dash.Dash(__name__)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
